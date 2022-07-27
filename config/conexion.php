a23df3111d925da661e6b14c920a1a8ed12a5e58
Alfredo <58349129+FredyAZEP@users.noreply.github.com>
2022-07-26 23:12:48 -0500

    Publicado en Heroku y conexion con la BD

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,10 @@
 
         protected function Conexion(){
             try{
-                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=andercode_webservice","root","");
+                //QA
+                // $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=andercode_webservice","root","");
+                // Produccion Heroku
+                $conectar = $this->dbh = new PDO("mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_5722f8f9d3eeba7","bde355ce3eb8c7","4290437d");
                 return $conectar;
             }catch (Exception $e){
                 print "¡Error DB!: " . $e->getMessage() . "<br/>";
